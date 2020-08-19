01c511aafc416dbafda7c7df07437ccca397d641
kagamiya <miyatei9dan@gmail.com>
2020-08-19 21:28:58 +0900

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,23 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # return user logged in now
+  # make an user session permanent
+  def remember(user)
+    user.remember_new_token
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # return the user associated with a cookie of remember token
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -17,8 +30,16 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # discard a permanent session
+  def forget(user)
+    user.forget_token
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # log out current user
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   before_save { email.downcase! }
 
@@ -8,11 +10,33 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-  
-  # return hash value for the passed string
+
+  # return hash value for the passed character string
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # return a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # remember new token in database for permanent session
+  def remember_new_token
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # return true when the passed token is equal to digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # discard a remember token
+  def forget_token
+    update_attribute(:remember_digest, nil)
   end
 end
