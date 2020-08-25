98a3c41fb405af2633fca5e2ba363dce8abcf517
kagamiya <miyatei9dan@gmail.com>
2020-08-25 22:19:23 +0900

    Implement icon_for users helper for user icon

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  # return a picture for an user icon
+  def icon_for(user, size: 80)
+    default_url = "sample.png"
+    picture_url = user.picture.url || default_url
+    image_tag(picture_url, alt: user.name, class: "user_icon")
+  end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -10,14 +10,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
-    # upload picture fo user icon
-      assert_select 'input[type=file]'
-      picture = fixture_file_upload('test/fixtures/sample_artwork.png', 'image/png')
     patch user_path(@user), params: { user: { name: "",
                                               email: "foo@invalid",
                                               password: "foo",
                                               password_confirmation: "bar",
-                                              picture: picture } }
+                                              picture: "" } }
     assert_template 'users/edit'
     assert_select "div.alert"                                          
   end
@@ -30,14 +27,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert session[:forwarding_url].nil?
     name = "foo bar"
     email = "foo@bar.com"
+    # upload picture for user icon
+    picture = fixture_file_upload('test/fixtures/sample.png', 'image/png')
     patch user_path(@user), params: { user: { name:name,
                                               email: email,
                                               password: "",
-                                              password_confirmation: "" } }
+                                              password_confirmation: "",
+                                              picture: picture } }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
     assert_equal name, @user.name
-    assert_equal email, @user.email                                              
+    assert_equal email, @user.email
+    assert @user.picture?
+    assert_equal "/uploads/user/picture/#{@user.id}/sample.png", @user.picture.url                                        
   end
 end
