24f0cacc9be86843589701c4e5ad446d2c23f364
kagamiya <miyatei9dan@gmail.com>
2020-09-19 16:33:20 +0900

    Improve post show

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ApplicationRecord
   accepts_nested_attributes_for :music, allow_destroy: true
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true
 end
